2ff17e9a46d335713c01a75ec73cc761b9d1358f
shiyonglong <yonglongshi@irissz.com>
2019-11-20 21:04:30 +0800

    修改表

--- src/main/java/com/example/skty/springboot/datePrepar/DateBaseImitate.java
+++ src/main/java/com/example/skty/springboot/datePrepar/DateBaseImitate.java
@@ -1,9 +1,46 @@
 package com.example.skty.springboot.datePrepar;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * 模拟数据库类（仅用于当前没有数据库测试用）
  */
-public class DateBaseImitate {
+public class  DateBaseImitate <K extends  Number> {
+
+    /**
+     * 所有的表
+     */
+    private Map<String,TableImitate<K,Object>> tables;
+
+    public DateBaseImitate() {
+        tables=new HashMap<>();
+    }
+
+    /**
+     * 使用表名创建一个表
+     * @param tableName
+     * @return
+     */
+    public boolean createTable(String tableName){
+        try {
+            tables.put(tableName, TableImitate.createTable(tableName));
+        }catch (Exception e){
+            return false;
+        }
+         return true;
+    }
+
+    /**
+     * 插入数据到指定的表中
+     * @return
+     */
+    public  boolean insertToTable(String tableName,K key,Object data){
+        TableImitate<K, Object> table = tables.get(tableName);
+        if (table == null) {
+            throw new IllegalArgumentException("表不存在,无法插入数据");
+        }
+        return   table.insertDate(key,data);
+    }
+
 }
--- src/main/java/com/example/skty/springboot/datePrepar/TableImitate.java
+++ src/main/java/com/example/skty/springboot/datePrepar/TableImitate.java
@@ -1,15 +1,47 @@
 package com.example.skty.springboot.datePrepar;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 表结构模拟
  */
-public class TableImitate {
+public  class  TableImitate<K extends Number,E> {
     private String tableName;//表名
-    private List<?> cloums;//所有列数据
-    private List<Number> indexs;//主键索引
+    private List<E> cloums;//所有列数据
+    private Map<K,E> indexes;//索引数据
+
+
+
+    public static <K extends  Number,E> TableImitate<K,E> createTable(String tableName){
+        if(tableName!=null&&tableName!=""){
+            return  new TableImitate<K,E>(tableName);
+        }else{
+            throw new IllegalArgumentException("表名不能为空");
+        }
+    }
+
+    /**
+     * 创建一个表
+     * @param tableName
+     */
+    private TableImitate(String tableName) {
+        this.tableName = tableName;
+        cloums= new ArrayList<E>();
+        indexes=new HashMap<>();
+    }
+
+    /**
+     * 往数据表种插入一条数据
+     * @return true插入成功 false，插入失败
+     */
+    public boolean insertDate(K key,E e){
+        if (e == null) {
+            return  false;
+        }
+        cloums.add(e);
+        indexes.put(key, e);
+        return true;
+    }
 
     public String getTableName() {
         return tableName;
@@ -23,15 +55,15 @@ public class TableImitate {
         return cloums;
     }
 
-    public void setCloums(List<?> cloums) {
+    public void setCloums(List<E> cloums) {
         this.cloums = cloums;
     }
 
-    public List<Number> getIndexs() {
-        return indexs;
+    public Map<K, E> getIndexes() {
+        return indexes;
     }
 
-    public void setIndexs(List<Number> indexs) {
-        this.indexs = indexs;
+    public void setIndexes(Map<K, E> indexes) {
+        this.indexes = indexes;
     }
 }
